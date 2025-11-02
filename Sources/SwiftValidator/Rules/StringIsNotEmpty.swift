32ec0f50989b1d0046d6761210bde808995f43f4
Kamaal Farah <kamaal.f1@gmail.com>
2025-11-02 16:23:57 +0100

    Adding string is not empty rule

---
+++ Sources/SwiftValidator/Rules/StringIsNotEmpty.swift
@@ -0,0 +1,8 @@
+//
+//  StringIsNotEmpty.swift
+//  swift-validator
+//
+//  Created by Kamaal M Farah on 02/11/2025.
+//
+
+public class StringIsNotEmpty: ValidateIsNotEmpty<String>, StringValidatableRule {}
